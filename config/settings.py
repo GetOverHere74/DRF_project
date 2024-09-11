2436f41064be5bd315aa0a1fe0fa3af4a3f02518
skyl1 <skyl1.maxla2@mail.ru>
2024-09-11 15:56:23 +0500

    Добавлена настройка авторизации пользователя

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users',
+    'materials',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
