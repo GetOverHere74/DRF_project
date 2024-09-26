534fdffdda069bf15d0a4b6a88125c06ae834c71
skyl1 <skyl1.maxla2@mail.ru>
2024-09-26 14:38:01 +0500

    Попытка вывести подписку

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from materials.models import Course, Lesson, Subscription
 from materials.validators import VideoValidator
@@ -13,6 +14,7 @@ class LessonSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons = LessonSerializer(source='lesson_count', many=True, read_only=True)
+    sign_up = SerializerMethodField(source='subscription_course', read_only=True)
 
     class Meta:
         model = Course
@@ -28,11 +30,16 @@ class SubscriptionSerializer(serializers.ModelSerializer):
 class CourseDetailSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(source='lesson_count', many=True, read_only=True)
+    subscription_course = serializers.SerializerMethodField()
     sign_up = SubscriptionSerializer(source='subscription_course', many=True, read_only=True)
 
     def get_lesson_count(self, course):
         return Lesson.objects.filter(course=course).count()
 
+    def get_sign_up(self, instance):
+        user = self.context['request'].user
+        return Subscription.objects.filter(user=user).filter(course=instance).exists()
+
     class Meta:
         model = Course
-        fields = ('lessons', 'title', 'description', 'lesson_count', 'image', 'owner', 'sign_up',)
+        fields = ('lessons', 'title', 'description', 'lesson_count', 'image', 'owner', 'subscription_course',)
